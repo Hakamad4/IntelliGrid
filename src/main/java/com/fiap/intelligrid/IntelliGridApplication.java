929f5ad30cc1f9228e72370e09d02723c0a60e34
maenrico <matheusx1272@gmail.com>
2023-05-24 20:36:55 -0300

    first commit

---
+++ src/main/java/com/fiap/intelligrid/IntelliGridApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.intelligrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliGridApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelliGridApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiap/intelligrid/IntelliGridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.intelligrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliGridApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
