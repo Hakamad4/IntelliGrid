03146f4921e134f13f788ca0878391733d717ca9
erik.hakamada <erik.hakamada@hotmail.com>
2023-06-26 23:55:39 -0300

    endereco-service - changind endereco response a record

--- src/main/java/com/fiap/intelligrid/domain/response/EnderecoResponse.java
+++ src/main/java/com/fiap/intelligrid/domain/response/EnderecoResponse.java
@@ -1,27 +1,11 @@
 package com.fiap.intelligrid.domain.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@NoArgsConstructor @AllArgsConstructor
-public class EnderecoResponse {
-
-	private String cep;
-
-	private String logradouro;
-
-	private String numero;
-
-	private String bairro;
-
-	private String cidade;
-
-	private String estado;
-
-	private String complemento;
+public record EnderecoResponse(String cep,
+							   String logradouro,
+							   String numero,
+							   String bairro,
+							   String cidade,
+							   String estado,
+							   String complemento) {
 
 }
