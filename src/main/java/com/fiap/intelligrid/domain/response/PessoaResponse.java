98478bc61d4915affedbb611f39c72853b90b88a
erik.hakamada <erik.hakamada@hotmail.com>
2023-06-27 07:50:21 -0300

    endereco-service - identation

--- src/main/java/com/fiap/intelligrid/domain/response/PessoaResponse.java
+++ src/main/java/com/fiap/intelligrid/domain/response/PessoaResponse.java
@@ -5,8 +5,8 @@ import com.fiap.intelligrid.domain.entity.Pessoa;
 
 import java.time.LocalDate;
 
-public record PessoaResponse(Long id, String nome, String email, LocalDate dataNascimento, Genero genero ) {
-    public PessoaResponse(Pessoa pessoa){
+public record PessoaResponse(Long id, String nome, String email, LocalDate dataNascimento, Genero genero) {
+    public PessoaResponse(Pessoa pessoa) {
         this(pessoa.getId(), pessoa.getNome(), pessoa.getEmail(), pessoa.getDataNascimento(), pessoa.getGenero());
     }
 }
